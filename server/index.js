535610895cfcfdbed07ac1e93a375d78080c3185
Jonathan Tredway <jptredway@gmail.com>
2018-09-08 15:01:12 -0500

    Add boilerplate

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.use('/api', routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
