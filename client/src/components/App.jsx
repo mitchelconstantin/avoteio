0188fa88083f2e92538f368e57e4ed7a631b8a15
Hunter Leeves <hunterleeves@gmail.com>
2018-09-10 14:04:03 -0500

    Functional rendering and search bar. Restructuring to include main/login/createRoom

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 import {data} from '../dummy_data.js';
 import SearchBar from './SearchBar.jsx';
+import Song from './Song.jsx'
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      songBank: 'I will eventually be an array of songs'
+      currentPage: 'Login',
+      songBank: []
     };
-    this.updateSongBank = this.updateSongBank.bind(this)
+    this.updateSongBank = this.updateSongBank.bind(this);
   }
 
   updateSongBank (songBank) {
     this.setState({
       songBank: songBank
     })
-    console.log('here is the songBank now:',songBank)
   }
+
+  componentDidMount () {
+    let songBank = []
+    for (let i = 0; i < 5; i++) {
+      console.log(data.tracks.items[i].name, '----',data.tracks.items[i].artists[0].name)
+      let song = {
+        title: data.tracks.items[i].name,
+        artist: data.tracks.items[i].artists[0].name
+      }
+      songBank.push(song)
+    }
+    this.setState({
+      songBank:songBank
+    })
+    
+  } 
+
   render() {
+    let component;
+    if(this.state.currentPage === 'Login') {
+      component = <Login />
+    } else if (this.state.currentPage === 'CreateRoom') {
+      component = <CreateRoom />
+    } else if (this.state.currentPage === 'Main') {
+      component = <Main />
+    }
     // console.log(data.tracks.items);
     return (
       <div>
         <h1>Howdy, World!</h1>
+        {/* {component} */}
+        {this.state.songBank.map( (song,index)=> <Song title={song.title} artist={song.artist} key={index}  / >)}
         <SearchBar updateSongBank={this.updateSongBank}/>
       </div>
     )
---
+++ client/src/components/Song.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Song = (props) => (
+
+      <div>
+          {console.log('Song props:',props)}
+          {props.title} by {props.artist}
+      </div>
+  
+    
+)
+
+
+
+export default Song
