833a0097a904d860bb6dad3a75365dedc8ee414b
shawnxa <shawnxacevedo@gmail.com>
2018-09-13 12:57:00 -0500

    in sync with dev branch

--- server/index.js
+++ server/index.js
@@ -2,13 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-const routes = require('./routes.js');
-=======
 const apiRoutes = require('./routes/apiRoutes');
 const authRoutes = require('./routes/authRoutes');
 const db = require('../database/index');
->>>>>>> 843d5365795628f252b1c6e5277a6ed3554ca47b
 
 const app = express();
 
